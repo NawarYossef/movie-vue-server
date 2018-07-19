574c60d5b9f339a10794b779b7e86e61c3feeb30
nawar yossef <nawaryossef2@gmail.com>
2018-07-19 15:33:05 -0500

    add GET, POST, DELETE endpoints

--- movies/router.js
+++ movies/router.js
@@ -5,19 +5,19 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 
-const { Job } = require("./model");
+const { Movie } = require("./model");
 // const jwtAuth = passport.authenticate("jwt", { session: false });
 
 router.use(jsonParser);
 
 // ============== GET endpoint ==============
 router.get("/", (req, res) => {
-  Job.find()
+  Movie.find()
     // call the `.serialize` instance method we've created in
     // models.js in order to only expose the data we want the API return.
-    .then(jobs => {
+    .then(movies => {
       res.json({
-        jobs: jobs.map(job => job.serialize())
+        movies: movies.map(movie => movie.serialize())
       });
     })
     .catch(err => {
@@ -26,27 +26,10 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
-  Job.findById(req.params.id)
-    .then(job => res.json(job.serialize()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: "something went horribly awry" });
-    });
-});
-
 // ============== POST endpoint ==============
 router.post("/", (req, res) => {
   const requiredFields = [
-    "companyName",
-    "companyLocation",
-    "positionTitle",
-    "companyType",
-    "salary",
-    "companyWebsite",
-    "linkJobDescription",
-    "jobStatus",
-    "notes"
+    "movieId"
   ];
 
   for (let i = 0; i < requiredFields.length; i++) {
@@ -58,61 +41,20 @@ router.post("/", (req, res) => {
     }
   }
 
-  Job.create({
-    companyName: req.body.companyName,
-    companyLocation: req.body.location,
-    positionTitle: req.body.positionTitle,
-    companyType: req.body.companyType,
-    salary: req.body.salary,
-    companyWebsite: req.body.companyWebsite,
-    linkJobDescription: req.body.linkJobDescription,
-    jobStatus: req.body.jobStatus,
-    notes: req.body.notes
-    // user: req.user.id
+  Movie.create({
+    movieId: req.body.movieId
   })
-    .then(job => res.status(201).json(job.serialize()))
+    .then(movie => res.status(201).json(movie.serialize()))
     .catch(err => {
       console.error(err);
       res.status(500).json({ error: "Something went wrong" });
     });
 });
 
-// ============== PUT endpoint ==============
-router.put("/:id", (req, res) => {
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    return res.status(400).json({
-      error: "Request path id and request body id values must match"
-    });
-  }
-
-  const updated = {};
-  const updatableFields = [
-    "companyName",
-    "companyLocation",
-    "positionTitle",
-    "companyType",
-    "salary",
-    "companyWebsite",
-    "linkJobDescription",
-    "jobStatus",
-    "notes"
-  ];
-
-  updatableFields.forEach(field => {
-    if (field in req.body) {
-      updated[field] = req.body[field];
-    }
-  });
-
-  Job.findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
-    .then(updatedJob =>   res.status(201).json(updatedJob.serialize()))
-    .catch(err => res.status(500).json({ message: "Something went wrong" }));
-});
-
 // ============== DELETE endpoint ==============
 router.delete("/:id", (req, res) => {
-    Job.findByIdAndRemove(req.params.id)
-    .then(job => res.status(204).end())
+    Movie.findByIdAndRemove(req.params.id)
+    .then(() => res.status(204).end())
     .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
 
